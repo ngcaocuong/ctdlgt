e35bfcb5a8d697e31d829ec6990d6ca202d8d877
ngcaocuong <70741973+ngcaocuong@users.noreply.github.com>
2021-10-03 20:58:14 +0700

    Add files via upload

---
+++ bai6.java
@@ -0,0 +1,95 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep) {
+        while (node != null) {
+            System.out.print(node.data);
+
+            node = node.next;
+
+            if (node != null) {
+                System.out.print(sep);
+            }
+        }
+    }
+
+class Result {
+
+    /*
+     * Complete the 'reversePrint' function below.
+     *
+     * The function accepts INTEGER_SINGLY_LINKED_LIST llist as parameter.
+     */
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+
+    public static void reversePrint(SinglyLinkedListNode head) {
+       SinglyLinkedListNode previous = null;
+       SinglyLinkedListNode next = null;
+       SinglyLinkedListNode current = head;
+ 
+       while(current != null) {
+           next = current.next;
+           current.next =  previous;
+           previous = current;
+           previous = next;
+            
+       }
+        
+       while(previous != null) {
+           System.out.println(previous.data);
+           previous = previous.next;
+            
+       }
+
+}
+}
+
+    private static final Scanner scanner = new Scanner(System.in);
